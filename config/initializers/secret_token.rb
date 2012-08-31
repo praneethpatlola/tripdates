d3b10add4e2328b1a62718cd4c66e7f43071a672
Praneeth Patlola <ppatlola@ppatlola.(none)>
2012-08-30 21:16:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripdates::Application.config.secret_token = 'c6dcfa08dee97546190e5f749d91eb601415f68fdf7abfbde7dfb484453264267e60b7c80d7553faae1999f6c83695fcdc260a1ea9571f7f2be54a1529c8b2b2'
